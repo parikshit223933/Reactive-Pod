dd59b4209812266632ad2630f46f96045e73134f
Parikshit Singh <pk223933@gmail.com>
2020-06-27 10:57:36 +0530

    styled buttons

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,12 +1,25 @@
 import React from 'react'
 
-const Buttons=(props)=>
+const Buttons = (props) =>
 {
     return (
         <div className="buttons-container">
-            <div className="center-circle">
+            <button className="center-circle">
                 <h2>Select</h2>
-            </div>
+            </button>
+
+            <button className="menu-button">
+                <i class="fas fa-bars"></i>
+            </button>
+            <button className="left-button">
+                <i class="fas fa-backward"></i>
+            </button>
+            <button className="right-button">
+                <i class="fas fa-forward"></i>
+            </button>
+            <button className="play-pause">
+                <i class="fas fa-play"></i> /<i class="fas fa-pause"></i>
+            </button>
         </div>
     );
 }
