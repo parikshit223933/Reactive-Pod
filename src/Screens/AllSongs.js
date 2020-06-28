fa45b5ea3bf264b430508676ce09ad4fe75e282c
Parikshit Singh <pk223933@gmail.com>
2020-06-28 16:07:37 +0530

    all songs list prepared and data retrieved from firebase storage

--- src/Screens/AllSongs.js
+++ src/Screens/AllSongs.js
@@ -1,14 +1,63 @@
 import React from 'react';
-
+import * as firebase from 'firebase';
 class AllSongs extends React.Component
 {
+    constructor()
+    {
+        super();
+        this.new_data_array=[]
+        this.state={
+            all_songs_list:[]
+        }
+    }
+    componentDidMount()
+    {
+        firebase.storage().ref().child('music').listAll()
+        .then((data)=>
+        {
+            data.items.forEach(async (ref)=>
+            {
+                await ref.getDownloadURL()
+                .then((url)=>
+                {
+                    this.new_data_array.push({name:ref.name, url:url})
+                    this.setState({
+                        all_songs_list:this.new_data_array
+                    });
+                })
+                .catch((error)=>
+                {
+                    console.log(error);
+                })
+            })
+        })
+        
+        .catch((error)=>
+        {
+            if(error)
+            {
+                console.log(error);
+            }
+        })
+    }
     render()
     {
         return (
-            <div>
-                <h1>
+            <div className="all-songs">
+                <h1 className="all-songs-heading">
                     All Songs
                 </h1>
+                <div className="all-songs-list">
+                    {this.state.all_songs_list.map((item)=>
+                    {
+                        console.log(item);
+                        return (
+                        <div>
+                            {item.name}
+                        </div>
+                        )
+                    })}
+                </div>
             </div>
         );
 
